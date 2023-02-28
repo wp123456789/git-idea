e041c93a9be2dcb3b63cbf2592e009eaccd2eb67
wp <wp@qq.com>
2023-02-28 22:32:11 +0800

    second commit

---
+++ src/main/java/org/example/Test.java
@@ -0,0 +1,8 @@
+package main.java.org.example;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello git1!");
+        System.out.println("Hello git1!");
+    }
+}
