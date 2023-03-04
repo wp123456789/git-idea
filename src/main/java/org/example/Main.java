7256008902681308d490278ca7a0875d06d31509
wp <wp@qq.com>
2023-03-04 21:22:37 +0800

    master commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello git!");
         System.out.println("Hello git4!");
+        System.out.println("master git4!");
     }
 }
